397350f552e4c2f3fec56e5ed15e0ce29b3a1d41
Nate J.M <nate@natejonah.com>
2024-04-19 22:15:25 +0100

    Set up routes for home and about pages

--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+    res.type('text/plain')
+    res.send('Meadowlark Travel')
+})
+
+app.get('/about', (req, res) => {
+    res.type('text/plain')
+    res.send('About Meadowlark Travel')
+})
+
 // Custom 404 page
 app.use((req, res) => {
     res.type('text/plain')
